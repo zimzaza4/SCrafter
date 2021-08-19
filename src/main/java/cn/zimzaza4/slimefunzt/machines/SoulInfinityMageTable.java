948e9d402a3b43fc15dc9addfe443dbe887ef831
zimzaza4 <1875162740@qq.com>
2021-08-19 13:27:42 +0800

    lol

--- src/main/java/cn/zimzaza4/slimefunzt/machines/SoulInfinityMageTable.java
+++ src/main/java/cn/zimzaza4/slimefunzt/machines/SoulInfinityMageTable.java
@@ -7,13 +7,11 @@ import io.github.mooy1.infinitylib.recipes.RecipeMap;
 import io.github.mooy1.infinitylib.recipes.RecipeOutput;
 import io.github.mooy1.infinitylib.recipes.ShapedRecipe;
 import io.github.mooy1.infinitylib.slimefun.AbstractTickingContainer;
-import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
 import io.github.thebusybiscuit.slimefun4.implementation.SlimefunPlugin;
 import io.github.thebusybiscuit.slimefun4.utils.ChestMenuUtils;
 import lombok.NonNull;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Objects.Category;
-import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
@@ -30,16 +28,13 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.BiConsumer;
-
 public class SoulInfinityMageTable extends AbstractTickingContainer {
     public final static int[] BOARD = new int []{
         0, 1, 2, 3, 4, 5, 6, 7, 8,
         9,         13     ,16,17,
-        18,        22,23  ,25,26,
-        27,        31,32  ,34,35
+        18,        22,23,24,25,26,
+        27,        31    ,34,35,
+        36,37,38,39,40,41,42,43,44
     };
     public final static int[] INPUT = new int []{
             10,11,12,19,20,21,28,29,30
@@ -80,7 +75,7 @@ public class SoulInfinityMageTable extends AbstractTickingContainer {
                             BlockStorage.checkID(block.getLocation().add(0,0,-2))=="ZIM_SOUL_TORCH"
                     ){
                         return true;
-                    }else {player.sendMessage("&c结构不完整"); return false;}
+                    }else {player.sendMessage("§c结构不完整"); return false;}
                 }else{return false;}
             }
 
@@ -96,8 +91,8 @@ public class SoulInfinityMageTable extends AbstractTickingContainer {
         for (int i : BOARD) {
             preset.addItem(i, new CustomItem(Material.BLUE_STAINED_GLASS_PANE), ChestMenuUtils.getEmptyClickHandler());
         }
-        preset.addItem(14, new CustomItem(Material.ORANGE_STAINED_GLASS_PANE, "输入灵魂合成剂>>>"));
-        preset.addItem(24, new CustomItem(Material.GREEN_STAINED_GLASS_PANE, "&c合成"));
+        preset.addItem(14, new CustomItem(Material.ORANGE_STAINED_GLASS_PANE, "输入灵魂合成剂>>>"), ChestMenuUtils.getEmptyClickHandler());
+        preset.addItem(32, new CustomItem(Material.GREEN_STAINED_GLASS_PANE, "&c合成"));
 
     }
 
@@ -115,10 +110,11 @@ public class SoulInfinityMageTable extends AbstractTickingContainer {
     protected void onBreak(@NonNull BlockBreakEvent event, @NonNull BlockMenu inv, @NonNull Location loc){
         inv.dropItems(loc, INPUT);
         inv.dropItems(loc, 33);
+        inv.dropItems(loc, 15);
     }
     @Override
     public void onNewInstance(@NonNull BlockMenu present, @NonNull Block b){
-            present.addMenuClickHandler(24, (p, slot,item, action)->{
+            present.addMenuClickHandler(32, (p, slot,item, action)->{
                 craft(b, present, p);
                 return false;
             });
