f02bdbff6c387821833640f1ba7dad7b7ffa2159
zimzaza4 <1875162740@qq.com>
2021-08-22 15:52:47 +0800

    lol

--- src/main/java/cn/zimzaza4/slimefunzt/World/VoidWorld.java
+++ src/main/java/cn/zimzaza4/slimefunzt/World/VoidWorld.java
@@ -65,7 +65,7 @@ public class VoidWorld extends ChunkGenerator {
                     data.setBlock(x1, y, z1, Material.BLACKSTONE);
                 }
 
-                int Fh = 4-height/30;
+                int Fh = 4-height/20;
 
                 for (int y = 71; y< Fh+71 ; y++){
                     data.setBlock(x1, y, z1, Material.STONE);
@@ -76,11 +76,13 @@ public class VoidWorld extends ChunkGenerator {
                 double noiseValue2 = landnoise.noise(realX, realZ ,3D, 0.5D);
                 int heig = (int) (noiseValue2 * 40 + 100);
                 if (heig<80) {
-
+                int Fh = 4-heig/20;
                     for (int y =170; y > heig+90 ; y--) {
                         data.setBlock(x1, y, z1, Material.STONE);
                     }
-                    data.setBlock(x1, 171 ,z1,Material.GRASS_BLOCK );
+                    for (int y = 171; y< Fh+171;y++) {
+                        data.setBlock(x1, y, z1, Material.GRASS_BLOCK);
+                    }
 
 
                 }
