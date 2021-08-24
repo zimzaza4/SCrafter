5135f7aa2d72fc36a2f7c79e4d72c7095265aec8
baoad <3200961931@QQ.com>
2021-08-24 13:55:32 +0800

    lol

--- src/main/java/cn/zimzaza4/slimefunzt/lists/Items.java
+++ src/main/java/cn/zimzaza4/slimefunzt/lists/Items.java
@@ -233,7 +233,7 @@ public class Items {
         public static SlimefunItemStack EM_soul = new SlimefunItemStack("EM_SOUL", Material.HEART_OF_THE_SEA,
                  "&b灵魂",
                  "",
-                 "&7闪耀着微弱的光");
+                 "&7&o闪耀着微弱的光");
 
         public static SlimefunItemStack EM_boss_soul = new SlimefunItemStack("EM_BOSS_SOUL", Material.HEART_OF_THE_SEA,
                  "&4亡魂",
